9333594302a0a81672a8d18c3ae5ee9cae4505c4
conconrob <connortprobertson@gmail.com>
2019-02-15 10:27:52 +0000

    Added cb functionality to getCharacters function

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     this.getCharacters('https://swapi.co/api/people');
   }
 
-  getCharacters = (URL, cb= ()=>{console.log('no callback')}) => {
+  getCharacters = (URL, cb= ()=>{}) => {// default cb is no callback
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
@@ -44,6 +44,7 @@ class App extends Component {
       .catch(err => {
         throw new Error(err);
       });
+      cb();
   };
 
   changePage = direction => {
