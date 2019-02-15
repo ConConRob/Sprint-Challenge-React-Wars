6201361803ab280e1a206fa0c5d31c1b848564ac
conconrob <connortprobertson@gmail.com>
2019-02-15 08:54:56 +0000

    CONE MVP -  styled the list items

--- starwars/src/components/List.jsx
+++ starwars/src/components/List.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ListItem from './ListItem';
-
+import './List.css'
 export default function List({list}){
     return (
-        <ul>
+        <ul className="list">
+            <ListItem listItem={{name:"Name", birth_year:"Year of Birth", height:"Height (cm)"}} />
             {
                 list.map((listItem, index )=> <ListItem key={index} listItem={listItem} />)
             }
--- starwars/src/components/ListItem.jsx
+++ starwars/src/components/ListItem.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 export default function List({listItem}){
     return (
-        <li className="listItem">{listItem.name}</li>
+        <li className="list-item">
+            <p className="list-item-name">{listItem.name}</p> 
+            <p className="list-item-birth-year" >{listItem.birth_year}</p>
+            <p className="list-item-height">{listItem.height}</p>
+        </li>
     )
 }
